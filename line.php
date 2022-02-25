6b23c0cb5acc4a378cf19145663edfbdcb1d58f5
warrapruch <100253694+warrapruch@users.noreply.github.com>
2022-02-25 15:08:45 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,7 +2,7 @@
   
 
 function send_LINE($msg){
- $access_token = 'Xtu1f5okc7oV8JIzUV1qSOwdpis/DTak64L8ZQqp8dOmVE21XSyNSsYiQarB49zfTiOSclLjGb9fMyG2Hpfs86+8teRdXyh/Lr17yJKBRAticVlAAO/3obQRiWTorbfWreMe50kU/ysmBy4tOdKmmAdB04t89/1O/w1cDnyilFU='; 
+ $access_token = 'eIxz8LWlLAlOqC1/lLL/e3LHq7vY84as/JY9ZqPCYn8lTrQPFcCQBKouwHoy0NnngTTAhxO5SIrmOQPHcSKpZ7nSauF+oOHz84VVRM3jPPWe9ayJrA5RLiK5raC635bWZl2jjj+I1E4ahqvdYpxVmAdB04t89/1O/w1cDnyilFU='; 
 
   $messages = [
         'type' => 'text',
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Uff90c022c39019b3ea05d43bb1008c28',
+        'to' => 'U94c08ddc8b6b198480ebfd699145aee6',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
