f564fa3f8aa7d85d86c972fe676fac37bd08b885
warrapruch <100253694+warrapruch@users.noreply.github.com>
2022-03-02 02:34:51 +0700

    Update pub.php

--- pub.php
+++ pub.php
@@ -45,7 +45,7 @@
       echo $response . "\r\n";
     return $response;
 }
-$Topic = "linebot";
-$lineMsg = "CHECK";
-getMqttfromlineMsg($Topic,$lineMsg);
+//$Topic = "linebot";
+//$lineMsg = "CHECK";
+//getMqttfromlineMsg($Topic,$lineMsg);
 ?>
